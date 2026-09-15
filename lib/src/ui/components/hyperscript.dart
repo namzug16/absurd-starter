edf67891b16ea2cb178cbd07c83f20c6511959dc
Gustavo Guzman <gustavo@namzug.dev>
2026-09-15 18:08:10 +0200

    fix: remove unused hyperscript commands

--- lib/src/ui/components/hyperscript.dart
+++ lib/src/ui/components/hyperscript.dart
@@ -2,10 +2,4 @@
 
 import "package:htmleez/htmleez.dart";
 
-HTML $disableFieldsetsAndShowLoadersOnHtmxRequest() => raw$("_")("on htmx:before:request toggle @disabled on <fieldset/> in me toggle .hidden on .loader in me until htmx:finally:request");
-
 HTML $_disableFieldsetsOnHtmxRequest() => raw$("_")("on htmx:before:request toggle @disabled on <fieldset/> in me until htmx:finally:request");
-
-HTML $_toggleLoaderOnHtmxRequest() => raw$("_")("on htmx:before:request from the closest <form/> toggle .hidden on .loader in me until htmx:finally:request from the closest <form/>");
-
-HTML $_toggleDisabledOnHtmxRequest() => raw$("_")("on htmx:before:request toggle @disabled on me until htmx:finally:request");
